1f33db26244888beb3a9a2b0a171ed1daf8b2548
sgnoo <dn2757@gmail.com>
2021-06-01 20:39:14 +0900

    Update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,23 @@
-require('dotenv').config();
+require('@eth-optimism/hardhat-ovm');
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const KOVAN_DEPLOYER_PRIV_KEY = process.env.KOVAN_DEPLOYER_PRIV_KEY;
 
 module.exports = {
-  solidity: '0.7.3',
+  solidity: '0.7.6',
+  ovm: {
+    solcVersion: '0.7.6',
+  },
   networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${KOVAN_DEPLOYER_PRIV_KEY}`],
+    },
+    optimism: {
+      url: '',
+      ovm: true,
     },
   },
 };
